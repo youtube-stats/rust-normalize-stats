0bbad771746e08946b4b7358e57a4476046eec21
Salvador Guzman <guzmansalv@gmail.com>
2019-05-27 21:16:54 -0700

    method for retrieving row

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,42 @@ struct Metric {
     time: chrono::DateTime<Local>,
     id: u32,
     subs: u32,
-    views: i64,
+    views: u64,
     videos: u32
+}
+
+fn get_row(conn: &Connection) -> Metric {
+    let query: &'static str = QUERY;
+    let rows: Rows = conn.query(query, &[]).unwrap();
+    let row: Row = rows.iter().last().unwrap();
+
+    let time: chrono::DateTime<Local> = row.get(0);
+
+    let id: i32 = row.get(1);
+    let id: u32 = id as u32;
+
+    let subs: i32 = row.get(2);
+    let subs: u32 = subs as u32;
+
+    let views: i64 = row.get(3);
+    let views: u64 = views as u64;
+
+    let videos: i32 = row.get(4);
+    let videos: u32 = videos as u32;
+
+    Metric {
+        time,
+        id,
+        subs,
+        views,
+        videos
+    }
+}
+
+impl Metric {
+    pub fn string(&self) -> String {
+        format!("Metric({},{},{},{},{})", self.time, self.id, self.subs, self.views, self.videos)
+    }
 }
 
 fn main() {
@@ -29,17 +63,8 @@ fn main() {
         println!("Inserted {} rows", count);
         count += 1;
 
-        let query: &'static str = QUERY;
-        let rows: Rows = conn.query(query, &[]).unwrap();
-        let row: Row = rows.iter().last().unwrap();
+        let row: Metric = get_row(&conn);
 
-        let time: chrono::DateTime<Local> = row.get(0);
-        let id: i32 = row.get(1);
-        let subs: i32 = row.get(2);
-        let views: i64 = row.get(3);
-        let videos: i32 = row.get(4);
-
-        println!("Retrieved (time={}, id={}, subs={}, views={}, videos={})",
-        time, id, subs, views, videos);
+        println!("Retrieved row {}", row.string());
     }
 }
